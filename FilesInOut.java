6a440d406b4cc7d4bf4ffe234b1b4900760c2f44
Callum Hunter <55032230+Stargate18@users.noreply.github.com>
2022-04-01 13:06:05 +0100

    Added basic file reading and input storage

--- FilesInOut.java
+++ FilesInOut.java
@@ -12,22 +12,35 @@ import java.lang.Number;
  */
 public class FilesInOut {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // Replace this with statements to set the file name (input) and file name (output).
         // Initially it will be easier to hardcode suitable file names.
-
+    	File file = new File(".\\src\\input.txt");
+    	String output = "output.txt";
+    	ArrayList<String> inputnames = new ArrayList<String>();
         // Set up a new Scanner to read the input file.
         // Processing line by line would be sensible here.
         // Initially, echo the text to System.out to check you are reading correctly.
         // Then add code to modify the text to the output format.
-
+    	
+    	Scanner in = new Scanner("");;
+    	try {
+			in = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+    	while (in.hasNextLine())
+    	{
+    	  inputnames.add(in.nextLine());
+    	  System.out.println(inputnames.get(inputnames.size() - 1));
+    	}
+    	
+    	
         // Set up a new PrintWriter to write the output file.
         // Add suitable code into the above processing (because you need to do this line by line also.
         // That is, read a line, write a line, loop.
 
         // Finally, add code to read the filenames as arguments from the command line.
-
-        System.out.println("You need to add your own code to do anything");
 
     } // main
 
